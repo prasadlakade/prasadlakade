b3325c80806d19c74400609da6844ebb65f5c04b
Prasad Lakade <prasadlakade@Prasads-MacBook-Air.local>
2020-10-26 01:41:13 +0530

    Updates

---
+++ precache-manifest.1d41b1c24d43d60ffc6fb48ae7d1631f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccba1cc2cb04c55d0ce071b58c6b1998",
+    "url": "/prasadlakade/index.html"
+  },
+  {
+    "revision": "e90c9284214b4ef20a07",
+    "url": "/prasadlakade/static/css/main.e9a0e55e.chunk.css"
+  },
+  {
+    "revision": "dfab0ae4b72a9416f481",
+    "url": "/prasadlakade/static/js/2.905be6c8.chunk.js"
+  },
+  {
+    "revision": "8d190e2d02f8e768921a63f56a741895",
+    "url": "/prasadlakade/static/js/2.905be6c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e90c9284214b4ef20a07",
+    "url": "/prasadlakade/static/js/main.852ff2f3.chunk.js"
+  },
+  {
+    "revision": "7f1a2c14728281fb1576",
+    "url": "/prasadlakade/static/js/runtime-main.09cfce64.js"
+  }
+]);
